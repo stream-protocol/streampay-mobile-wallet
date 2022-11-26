94ce40f7b6e4529befb5e274b8871915def5c8cd
Fyfy Dev <105049869+stream-protocol@users.noreply.github.com>
2022-11-26 14:32:24 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,34 @@
+## Stream Mobile Wallet
+
+A Stream Mobile Wallet in flutter
+
+Features:
+- Add New Wallet
+- Import wallets with it's seedphrase (devnet only for now)
+- Create new wallets (devnet only for now)
+
+
+### Building android
+
+```
+flutter build android
+```
+
+### Building iOS
+
+```
+flutter build ios
+```
+### Future Developments
+
+```
+- Add Stream Token (STR) support
+- Cross-Chain Payments
+- POS Payments, commerce support
+- Better Wallet Features
+- Swap (crypto)
+- Wallet features
+- Solana Pay, QR-code support
+- NFC payment features
+- Chat features, peer-to-peer payment transactions
+- Pay Crypto, bank card features
---
+++ lib/main.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:streampay/pages/create_wallet.dart';
+import 'package:streampay/pages/import_wallet.dart';
+import 'package:streampay/pages/manage_accounts.dart';
+import 'package:streampay/pages/new_wallet.dart';
+import 'package:streampay/pages/pages.dart';
+import 'package:streampay/pages/passphrase.dart';
+import 'package:streampay/pages/password.dart';
+import 'package:streampay/pages/pay.dart';
+import 'package:streampay/pages/pin_code.dart';
+import 'package:streampay/pages/receive.dart';
+import 'package:streampay/pages/send.dart';
+import 'package:streampay/pages/splash.dart';
+import 'package:streampay/pages/my_wallet.dart';
+import 'package:worker_manager/worker_manager.dart';
+import 'state/store.dart' show AppState, StateWrapper, createStore;
+// import 'pages/home.dart';
+import 'pages/home_sub.dart';
+import 'pages/account_selection.dart';
+
+main() async {
+  LicenseRegistry.addLicense(() async* {
+    final license = await rootBundle.loadString('fonts/OFL.txt');
+    yield LicenseEntryWithLineBreaks(['fonts'], license);
+  });
+
+  await Executor().warmUp();
+  StateWrapper store = await createStore();
+  runApp(App(store));
+}
+
+class App extends StatelessWidget {
+  final StateWrapper store;
+  late String initialRoute = '/splash';
+
+  App(this.store) {
+    /*
+     * If there isn't any account created yet, then launch Getting Started Page
+     */
+    if (store.state.accounts.length == 0) {
+      // this.initialRoute = '/account_selection';
+      this.initialRoute = '/splash';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StoreProvider<AppState>(
+      store: this.store,
+      child: MaterialApp(
+        title: 'streampay',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        initialRoute: this.initialRoute,
+        routes: {
+          '/nav': (context) => PagesWidget(store: this.store,),
+          '/splash': (context) => SplashPage(),
+          '/home': (context) => HomeSubPage(
+            store: this.store,
+          ),
+          '/PinCode': (context) => PinCodeWidget(),
+          '/Password': (context) => PasswordWidget(),
+          '/Passphrase': (context) => PassphraseWidget(),
+          '/account_selection': (context) =>
+              AccountSelectionPage(store: this.store),
+          '/Receive': (context) => ReceiveWidget(store: this.store),
+          '/Send': (context) => SendWidget(store: this.store),
+          '/Pay': (context) => PayWidget(store: this.store),
+          '/watch_address': (context) => WatchAddress(store: this.store),
+          '/new_wallet': (context) => NewWallet(store: this.store),
+          '/create_wallet': (context) => CreateWallet(store: this.store),
+          '/import_wallet': (context) => ImportWallet(store: this.store),
+          // '/manage_accounts': (context) =>
+          //     ManageAccountsPage(store: this.store),
+        },
+      ),
+    );
+  }
+}
